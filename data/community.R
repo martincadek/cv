2e5fcc905fb7a3938972e4d3ba0d8a9595a767bd
Martin <marti.cadek@gmail.com>
2020-06-21 00:43:26 +0100

    updated

---
+++ data/community.R
@@ -0,0 +1,14 @@
+# Community ---------------------------------------------------------------
+community <- tribble(
+     ~ role, ~ activity, ~ location, ~ dates, ~ details,
+     "Co-organiser", "Leeds R Meetups", "Leeds, England", "2017 - Present", 
+     "As an R enthusiast, I have been involved in organising the meetups at Leeds Beckett University, and later in the general area of Leeds",
+     
+     "Co-organiser", "Leeds R Meetups", "Leeds, England", "2017 - Present", 
+     "https://cli.re/E59kPe",
+     
+     "Lecturer", "Qualtrics workshops", "Leeds", "2018", 
+     "Delivering series of free workshops about survey development for research staff at Leeds Beckett University"
+     
+     # End of tibble
+     )
---
+++ data/education.R
@@ -0,0 +1,12 @@
+# Education ---------------------------------------------------------------
+education <- tribble(
+     ~ degree, ~ uni, ~ loc, ~ dates, #~ details,
+     "PhD degree, Public Health", "Leeds Beckett University",  "Leeds, England", "2017 - ongoing",
+     
+     "Master's degree, Psychology", "Charles University",  "Prague, Czechia", "2013 – 2016",
+     
+     "Bachelor's degree, Psychology", "Charles University",  "Prague, Czechia", "2010 – 2013"
+     
+     # End of tibble
+     ) 
+
