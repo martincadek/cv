78cabfe44482ade03675d73f5307023cbaf337e3
Martin <marti.cadek@gmail.com>
2020-06-21 01:48:59 +0100

    update

--- data/education.R
+++ data/education.R
@@ -3,9 +3,9 @@ education <- tribble(
      ~ degree, ~ uni, ~ loc, ~ dates, #~ details,
      "PhD degree, Public Health", "Leeds Beckett University",  "Leeds, England", "2017 - ongoing",
      
-     "Master's degree, Psychology", "Charles University",  "Prague, Czechia", "2013 – 2016",
+     "Master's degree, Psychology", "Charles University",  "Prague, Czechia", "2013 - 2016",
      
-     "Bachelor's degree, Psychology", "Charles University",  "Prague, Czechia", "2010 – 2013"
+     "Bachelor's degree, Psychology", "Charles University",  "Prague, Czechia", "2010 - 2013"
      
      # End of tibble
      ) 
