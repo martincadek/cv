e0e0f0b9a532b39a1ab056b4cada15a9ab2018df
Martin <marti.cadek@gmail.com>
2020-07-19 22:38:34 +0100

    update

--- data/community.R
+++ data/community.R
@@ -1,11 +1,14 @@
 # Community ---------------------------------------------------------------
 community <- tribble(
      ~ role, ~ activity, ~ location, ~ dates, ~ details,
-     "Co-organiser", "Leeds R Meetups", "Leeds, England", "2017 - Present", 
-     "As an R enthusiast, I have been involved in organising the meetups at Leeds Beckett University, and later in the general area of Leeds",
+     "Co-organiser", "Leeds R Meetups", "Leeds, England", "2019 - Present", 
+     "As an R enthusiast, I have been involved in organising the R meetups in Leeds",
      
-     "Co-organiser", "Leeds R Meetups", "Leeds, England", "2017 - Present", 
+     "Co-organiser", "Leeds R Meetups", "Leeds, England", "2019 - Present", 
      "https://cli.re/E59kPe",
+     
+     "Organiser", "Leeds Beckett R Meetups", "Leeds, England", "2018", 
+     "I held a series of R meetups to provide R training to other PhD students, and supported the statistical analysis that was essential for other research projects.",
      
      "Lecturer", "Qualtrics workshops", "Leeds", "2018", 
      "Delivering series of free workshops about survey development for research staff at Leeds Beckett University"
--- data/courses.R
+++ data/courses.R
@@ -4,15 +4,18 @@ courses <- tribble(
      "Online", "DataCamp", "https://www.datacamp.com", "2018 - ongoing",
      "Introduction to SQL, Python for R Users, Foundations of Inference, Inference for Linear Regression in R, Inference for Numerical Data in R, Inference for Categorical Data in R, Case Study: Exploratory Data Analysis in R, Cleaning Data in R, Structural Equation Modeling with lavaan in R, Factor Analysis in R, Machine Learning with caret in R, Introduction to Machine Learning, Multiple and Logistic Regression in R, Introduction to Importing Data in R, Correlation and Regression in R, Introduction to Data in R, Reporting with R Markdown, Exploratory Data Analysis in R, Data Visualization with ggplot2 (Part 1), Data Visualization in R, Joining Data in R with dplyr, Data Manipulation in R with dplyr, Importing & Cleaning Data in R: Case Studies, Intermediate Importing Data in R, Intermediate R: Practice, Intermediate R, Introduction to R",
      
-     "Online", "Agrocampus Ouest", "https://cli.re/5Azz8D", "2017", 
-     "Exploratory Multivariate Data Analysis (MOOC) by François Husson, Professor of statistics. The course focused on four essential and basic methods: principal component analysis (PCA) to handle quantitative variables, correspondence analysis (CA) and multiple correspondence analysis (MCA) to handle categorical variables and clustering, and an extension to Multiple Factor Analysis (MFA) to analyse a more complex dataset that is structured by groups",
-     
      "Course", "University of Leeds", "Leeds, England", "2018", 
      "Summer School of Causal inference with observational data: the challenges and pitfalls (5 days course). This five-day summer school offered state-of-the-art training in the analysis of observational data for causal inference. By exploring the philosophy and utility of directed acyclic graphs (DAGs) participants learn to recognise and avoid a range of common pitfalls in the analysis of complex causal relationships, including the longitudinal analyses of change, mediation, nonlinearity and statistical interaction",
      
+     "Online", "Agrocampus Ouest", "https://cli.re/5Azz8D", "2017", 
+     "Exploratory Multivariate Data Analysis (MOOC) by François Husson, Professor of statistics. The course focused on four essential and basic methods: principal component analysis (PCA) to handle quantitative variables, correspondence analysis (CA) and multiple correspondence analysis (MCA) to handle categorical variables and clustering, and an extension to Multiple Factor Analysis (MFA) to analyse a more complex dataset that is structured by groups",
+     
+     "Course", "Leeds Beckett University", "Leeds, England", "2017",
+     "Understanding statistics and data analysis in R. This five-day course led by Professor Clive Beggs aimed to equip students and staff with the understanding necessary to undertake post-graduate research. The course covered topics such as variance & probability, parametric hypothesis testing, non-parametric testing, generalized linear models, bootstrapping & Monte Carlo techniques, and introduction to machine learning.",
+     
      "Course", "University of Cambridge", "Cambridge, England", "2016", 
      "Certified IRT and CAT course using Concerto at Psychometric Centre (3 days course). This course focused on producing online state-of-the-art psychometric tests, as well as an introduction to the Item Response Theory (IRT), Computer Adaptive Testing (CAT), and the open-source online adaptive test development platform, Concerto"
-     
+
      # End of tibble
      )
 
