6ae45d0cbf0379675a62d879f5618d2e24accb9d
martin_cadek <marti.cadek@gmail.com>
2023-11-19 23:36:46 +0000

    Add installation for renv package

--- setup.R
+++ setup.R
@@ -1,6 +1,11 @@
 # setup -------------------------------------------------------------------
-# install.packages('vitae')
-# install.packages("devtools")
-# devtools::install_github("mitchelloharawild/vitae")
-library(vitae)
-library(tinytex)
+# do not forget to install tinytext!
+# tinytex::install_tinytex()
+# also look at: https://yihui.org/tinytex/r/#debugging
+# also read https://bookdown.org/yihui/rmarkdown-cookbook/install-latex-pkgs.html
+
+
+if(!requireNamespace("renv", quietly = TRUE)) {
+  install.packages("renv")
+}
+
