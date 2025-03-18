f3f7a0567debef5b41a997e398591c03c8b66820
martin_cadek <marti.cadek@gmail.com>
2025-03-18 23:49:48 +0000

    chore: Add libraries.R for package management and update cadek_cv.Rmd formatting

---
+++ libraries.R
@@ -0,0 +1,15 @@
+# renv libraries
+library(httpgd)
+library(languageserver)
+library(vitae) 
+# https://pkg.mitchelloharawild.com/vitae/
+# https://github.com/tallguyjenks/CV (inspired by)
+# http://chrisumphlett.com/posts/vitae-package/ (inspired by)
+# Additional R Packages:
+library(tibble)
+library(dplyr)
+library(tinytex)
+library(glue)
+library(here)
+library(tidyverse)
+library(rmarkdown)
