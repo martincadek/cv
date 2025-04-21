860bc3e973939beab148d177b9a72e1c7f7f6cd1
martin_cadek <marti.cadek@gmail.com>
2025-04-21 15:17:45 +0100

    chore: Add skills.R file to document key skills and competencies

---
+++ data/skills.R
@@ -0,0 +1,19 @@
+# Skills -----------------------------------------------------------------
+skills <- tribble(
+     ~ category, ~ activity, ~ location, ~ dates, ~ details,
+     "Programming & Tools", "", "", "",
+     "Proficient in R, Shiny, Git, SQL, SAS, Python, NVivo, Qualtrics",
+
+     "Data Analysis", "", "", "",
+     "Expert in statistical modelling and quantitative research methodologies",
+
+     "Project Management", "", "", "",
+     "Efficient in managing timelines, scope, and resources for UX research projects",
+
+     "Client Relations", "", "", "",
+     "Skilled in maintaining client relationships and aligning research with client goals",
+
+     "UX Research & Communication", "", "", "",
+     "Proficient in UX research, data visualisation, and presenting research findings concisely"
+        # End of tribble
+     )
