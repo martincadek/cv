d5aed19a50c1a765af32e1f804274c2356046e20
martin_cadek <marti.cadek@gmail.com>
2025-04-21 15:38:45 +0100

    chore: Add PgCert in Statistics and Data Science to education.R

--- data/education.R
+++ data/education.R
@@ -1,6 +1,8 @@
 # Education ---------------------------------------------------------------
 education <- tribble(
      ~ degree, ~ uni, ~ loc, ~ dates, #~ details,
+     "PgCert", "Statistics and Data Science", "Trinity College Dublin", "Dublin, Ireland", "2024 - 2025",
+
      "PhD degree, Psychology", "Leeds Beckett University",  "Leeds, England", "2017 - 2022",
      
      "Master's degree, Psychology", "Charles University",  "Prague, Czechia", "2013 - 2016",
