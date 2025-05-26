451c5fcdb11497fdca611cf48d94f810ee0fbde4
martin_cadek <marti.cadek@gmail.com>
2025-05-26 11:28:58 +0100

    chore: Update skills section for clarity and consistency

--- data/skills.R
+++ data/skills.R
@@ -2,18 +2,18 @@
 skills <- tribble(
      ~ category, ~ activity, ~ location, ~ dates, ~ details,
      "Programming & Tools", "", "", "",
-     "Proficient in R, Shiny, Git, SQL, SAS, Python, NVivo, Qualtrics",
+     "Proficient in R, Shiny, Git, SQL, SAS, Python",
 
      "Data Analysis", "", "", "",
      "Expert in statistical modelling and quantitative research methodologies",
 
      "Project Management", "", "", "",
-     "Efficient in managing timelines, scope, and resources for UX research projects",
+     "Efficient in managing timelines, scope, and resources for research projects",
 
      "Client Relations", "", "", "",
      "Skilled in maintaining client relationships and aligning research with client goals",
 
-     "UX Research & Communication", "", "", "",
-     "Proficient in UX research, data visualisation, and presenting research findings concisely"
+     "Research & Communication", "", "", "",
+     "Proficient in research, data visualisation, and presenting research findings concisely"
         # End of tribble
      )
